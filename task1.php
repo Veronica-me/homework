39672ab0300bac9e154a2e5bdeb06cf2fb731116
Veronica-me <54118252+Veronica-me@users.noreply.github.com>
2019-08-17 14:39:10 +0300

    Add files via upload

---
+++ task1.php
@@ -0,0 +1,5 @@
+<?php
+
+$a = 2019;
+echo $a.'<br>';
+echo gettype($a);
---
+++ task2.php
@@ -0,0 +1,5 @@
+<?php
+define("NAME", "Вероника");
+$age = 35;
+echo "Меня зовут ".NAME.'<br>'."Мой возраст ".$age." лет";
+$age = null;
