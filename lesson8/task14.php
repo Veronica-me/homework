85a39e85cfb660479532081810730b6c9788fb1f
Veronica-me <54118252+Veronica-me@users.noreply.github.com>
2019-09-06 02:30:35 +0300

    Create task14.php

---
+++ lesson8/task14.php
@@ -0,0 +1,30 @@
+<?php
+require_once 'Worker.php';
+
+$worker1 = new Worker();
+$worker1->setName('Иван');
+$worker1->setAge(25);
+$worker1->setSalary(1000);
+
+$worker2 = new Worker();
+$worker2->setName('Вася');
+$worker2->setAge(26);
+$worker2->setSalary(2000);
+
+
+var_dump($worker1);
+var_dump($worker2);
+
+echo '<br>';
+$sum1 = $worker1->getSalary() + $worker2->getSalary();
+
+echo 'Сумма зарплат: '.$sum1.'<br>';
+$sum2 = $worker1->getAge() + $worker2->getAge();
+echo 'Сумма возрастов: '.$sum2.'<br>';
+
+showWorkers($worker1);
+showWorkers($worker2);
+
+function showWorkers(Worker $worker){
+    printf('Работник: %s; Возраст: %s <br>', $worker->getName(), $worker->getAge());
+}
