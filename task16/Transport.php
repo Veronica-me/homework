d7100d624fcebba3d8811d473caaea095b91cb62
Veronica-me <54118252+Veronica-me@users.noreply.github.com>
2019-10-04 01:25:32 +0300

    Add files via upload

--- task16/Transport.php
+++ task16/Transport.php
@@ -4,7 +4,7 @@ abstract class Transport
 {
     protected $status = 'stop'; //статус транспорта stop/go/load/unload
     protected $maxFilling; //допустимое количество груза/пассажиров
-    protected static $filling = 0;
+    protected static $filling = 0; //загружаемая величина
 
     public function __construct(int $maxFilling)
     {
@@ -13,7 +13,21 @@ abstract class Transport
 
     public function changeStatus (string $status)
     {
+        if ($status === 'stop' && self::$filling !=0 ){
+            echo '<br> Транспорт заполнен, требуется разгрузка <br>';
+        } elseif ($status === 'stop' && self::$filling =0) {
+            $this->status = $status;
+        } elseif ($status === 'go' || $status === 'load' || $status === 'unload'){
+            $this->status = $status;
+        } else {
+            echo '<br>Выбран неверный статус!!<br>';
+        }
 
+    }
+
+    public function getStatus(): string
+    {
+        return $this->status;
     }
 
 }
