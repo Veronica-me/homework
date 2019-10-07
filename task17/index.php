0182dfec24f9b782f9748e8334b866ee39fedf7f
Veronica-me <54118252+Veronica-me@users.noreply.github.com>
2019-10-08 02:40:52 +0300

    Update index.php

--- task17/index.php
+++ task17/index.php
@@ -1,5 +1,33 @@
 <?php
 require_once 'csv/Read.php';
 require_once 'csv/Write.php';
-require_once 'InterfaceReadUser.php';
-require_once 'InterfaceWriteUser.php';
+//require_once 'InterfaceReadUser.php';
+//require_once 'InterfaceWriteUser.php';
+
+$file = 'csv/data.csv';
+$newUser = [$_GET['name'], $_GET['dateOfBirthday']];
+$addUser = new Write($file);
+$addUser->setFile($newUser);
+
+$csvData = new Read($file);
+$getCsv = $csvData->getFileData();
+
+
+
+?>
+<!DOCTYPE html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=windows-1251">
+    <title>Работа с файлами</title>
+</head>
+<body>
+<p>Добавьте пользователя и дату рождения :</p>
+<form action="index.php" method="get">
+    Введите имя: <input type="text" name="name" /><br>
+    Введите дату рождения: <input type="date" name="dateOfBirthday" /><br>
+    <input type="submit" />
+</form>
+
+</body>
+
+</html>
