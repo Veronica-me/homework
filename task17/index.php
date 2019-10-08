dbc2a05ec2a0d8bff0c984502e6de99c84265e5d
Veronica-me <54118252+Veronica-me@users.noreply.github.com>
2019-10-08 20:39:59 +0300

    Update index.php

--- task17/index.php
+++ task17/index.php
@@ -1,7 +1,8 @@
 <?php
+require_once 'InterfaceReadUser.php';
 require_once 'csv/Read.php';
 require_once 'csv/Write.php';
-//require_once 'InterfaceReadUser.php';
+
 //require_once 'InterfaceWriteUser.php';
 
 $file = 'csv/data.csv';
